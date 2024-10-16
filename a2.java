95c3787fdec5ba2249131f7e1f09a4b8b5f299ad
JustisSmith <149518111+JustisSmith@users.noreply.github.com>
2024-10-16 16:59:20 -0400

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,14 @@
+// Justis Smith
+// CMSC 435 Assignment 2
+import java.util.Scanner;
+
+public class a2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        sc.close();
+    }
+}
